872fc836ec7d120f2c9ed406dc222c9e3796d881
panage-jp <joojoojoo6217@gmail.com>
2019-09-19 10:38:06 +0900

    edit index view

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -5,29 +5,56 @@ $(document).on('turbolinks:load', function () {
 
     }, 600);
   });
+
+
+  //セカンドヘッダースライドイン
+
+  $(".nav-link")
+    .mouseover(function () {
+      if ($(this).attr("data-fader") != $(".activative").attr("data-fader")) {
+        $(".fader").slideUp("fast");
+        $(".activative").removeClass("activative")
+      }
+    })
+  $(".navbar")
+    .mouseover(function () {
+      $(".fader").slideUp("fast");
+      $(".activative").removeClass("activative")
+    })
+  $(".carousel-item")
+    .mouseover(function () {
+      $(".fader").slideUp("fast");
+      $(".activative").removeClass("activative")
+    });
+
+
+
+
+
+  $("#pick_up")
+    .mouseover(function () {
+      $("#pick_up_fader").slideDown("fast");
+      $("#pick_up_fader").addClass("activative");
+    });
+
+
+  $("#shop")
+    .mouseover(function () {
+      $("#shop_fader").slideDown("fast");
+      $("#shop_fader").addClass("activative");
+    })
+
+
+
+
+  //pickeddogホバー処理
+
   $(".picked_dog")
     .mouseover(function () {
       $(this).find(".card-img-overlay").removeClass("hide");
     })
     .mouseout(function () {
       $(this).find(".card-img-overlay").addClass("hide");
-    });
-
-
-  $("#pick_up")
-    .mouseover(function () {
-      $("#pick_up_fader").slideDown();
-    })
-    .mouseout(function () {
-      $("#pick_up_fader").slideUp("fast");
-    });
-
-  $("#shop")
-    .mouseover(function () {
-      $("#shop_fader").slideDown();
-    })
-    .mouseout(function () {
-      $("#shop_fader").slideUp("fast");
     });
 
 });

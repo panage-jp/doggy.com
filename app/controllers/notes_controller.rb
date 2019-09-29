363668db85852dd1f9c6d6aea12faacc3b363111
panage-jp <joojoojoo6217@gmail.com>
2019-09-29 23:06:51 +0900

    create users controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,7 @@ class NotesController < ApplicationController
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.all
+    
   end
 
   # GET /notes/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "notes#index"
-  resources :notes
+  resources :notes, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/users/:id" => "users#show"
   get "signin" => "notes#signin"
   get "login" => "notes#login"
   get "post" => "notes#post"

a41e4a38786ee9705120b074f4faf096c5cab986
panage-jp <joojoojoo6217@gmail.com>
2019-09-29 21:07:56 +0900

    introduce login and sifn up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name,
+    length: { minimum: 1, maximum: 10 },
+    presence: true
+  
+  validates :introduction,
+    length: {maximum: 140}
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

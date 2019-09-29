2792e81916154741e6623535ff44f3e42b51bb52
panage-jp <joojoojoo6217@gmail.com>
2019-09-29 14:06:10 +0900

    edit some views

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,4 +7,8 @@ $(document).on('turbolinks:load', function () {
   $("#file_image").click(function () {
     $("#file_image_input").trigger('click');
   });
+
+  $("#dog_image").click(function () {
+    $("#dog_image_input").trigger('click');
+  });
 });

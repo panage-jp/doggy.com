b48fbe3e6e5c4f38983c460055b6db1a7ee1feaf
panage-jp <joojoojoo6217@gmail.com>
2019-10-01 17:15:18 +0900

    create tweet_tag_table

---
+++ app/models/tweet_tag.rb
@@ -0,0 +1,4 @@
+class TweetTag < ApplicationRecord
+  belongs_to :post
+  belongs_to :tag
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_053116) do
+ActiveRecord::Schema.define(version: 2019_10_01_080855) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,21 @@ ActiveRecord::Schema.define(version: 2019_10_01_053116) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweet_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_tweet_tags_on_post_id"
+    t.index ["tag_id"], name: "index_tweet_tags_on_tag_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,4 +78,6 @@ ActiveRecord::Schema.define(version: 2019_10_01_053116) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tweet_tags", "posts"
+  add_foreign_key "tweet_tags", "tags"
 end

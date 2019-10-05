7948f7ba1049ee62cef32e7f26c67970a4666c9e
panage-jp <joojoojoo6217@gmail.com>
2019-10-06 01:05:07 +0900

    delete space of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   root "notes#index"
   resources :notes, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :likes, :only => [:create, :destroy]
+  end
   resources :users, :only => [:show,:index]
   resources :dogs, :only => [:create, :destroy, :update]
   resources :comments, :only => [:create, :destroy]
-  resources :likes, :only => [:create, :destroy]
 end

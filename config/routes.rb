598baffdda623b1262f2281510a4566d12e1cf19
panage-jp <joojoojoo6217@gmail.com>
2019-10-08 10:02:51 +0900

    change root view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'posts/index'
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
-  root "notes#index"
+  root "posts#index"
   resources :notes, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
